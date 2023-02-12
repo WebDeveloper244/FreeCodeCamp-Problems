30eae6b7311d76db7775100636ba6ccff01272e4
WebDevolper1122 <sb2137842@gmail.com>
2023-02-13 02:07:31 +0500

    commit

--- All-Problems/Functional-Programing/higherOrderFunction.js
+++ All-Problems/Functional-Programing/higherOrderFunction.js
@@ -4,13 +4,8 @@ Use Higher Order Function map,filter,reduce to solve complex problem
 
 const squareList = arr => {
     // Only change code below this line
-    return arr;
+    return arr.filter((num)=> Number.isInteger(num) && num > 0).map((sqr)=>sqr * sqr)
     // Only change code above this line
   };
-
-  
-
-
-  
   const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
   console.log(squaredIntegers);
