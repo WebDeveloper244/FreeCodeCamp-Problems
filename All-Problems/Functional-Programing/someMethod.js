c6b65b2ac698dcaf8405289b754fd8ef97551fef
WebDevolper1122 <sb2137842@gmail.com>
2023-02-13 14:11:39 +0500

    commit

---
+++ All-Problems/Functional-Programing/someMethod.js
@@ -0,0 +1,13 @@
+/*  
+Use the some Method to Check that Every Element in an Array Meets a Criteria
+*/
+
+function checkPositive(arr) {
+    // Only change code below this line
+  
+    return arr.some((positiveNbr)=>positiveNbr>0)
+  
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]));
---
+++ All-Problems/Functional-Programing/sumOfNumberInRange.js
@@ -0,0 +1,13 @@
+/*  
+Sum All Numbers in a Range
+*/
+
+function sumAll(arr) {
+    let result = 0;
+    for(let i=Math.min(...arr); i<=Math.max(...arr); i++){
+      result +=i
+    }
+    return result
+  }
+  
+  console.log(sumAll([1, 4]));
