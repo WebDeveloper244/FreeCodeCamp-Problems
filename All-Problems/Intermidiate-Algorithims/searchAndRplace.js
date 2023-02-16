fa9248e05b1ec11f63c2899504e598fbd4353a00
WebDevolper1122 <sb2137842@gmail.com>
2023-02-16 22:08:36 +0500

    commit

--- All-Problems/Intermidiate-Algorithims/searchAndRplace.js
+++ All-Problems/Intermidiate-Algorithims/searchAndRplace.js
@@ -4,9 +4,17 @@ Search and Replace
 
 function myReplace(str,before,after){
   
-    
+    if(before[0] === before[0].toUpperCase()){
+        after = after[0].toUpperCase() + after.slice(1)
+    }
+    if(before[0] === before[0].toLowerCase()){
+        after = after[0].toLowerCase() + after.slice(1)
+    }
+
+    return str.replace(before,after)
+
 }
 
-let res = myReplace("Let us go to the store", "store", "mall");
+let res = myReplace("Let us go to the Store", "Store", "mall");
 
-res
+res;
