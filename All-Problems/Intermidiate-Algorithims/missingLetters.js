2378ace7759e4f707f6a56471f47117c18d5fec2
WebDevolper1122 <sb2137842@gmail.com>
2023-02-16 22:51:39 +0500

    commit

--- All-Problems/Intermidiate-Algorithims/missingLetters.js
+++ All-Problems/Intermidiate-Algorithims/missingLetters.js
@@ -3,20 +3,19 @@ Missing Letters
 */
 
 function fearNotLetter(str) {
-     let createArr = str.split('');
+    let createArr = str.split('');
     let alphabets = "abcdefghijklmnopqrstuvwxyz";
     let startingPostion = alphabets.indexOf(str[0]);
     let focusedElement  = alphabets.slice(startingPostion);
-    focusedElement
-     createArr.map((ele,i) => {
-        console.log(ele, focusedElement[i]);
+    let res = createArr.map((ele,i) => {
         if(ele !== focusedElement[i]){
             return focusedElement[i]
         }
-     })
-    return createArr
+     }).join('')
+
+    return res
   }
   
  let result =  fearNotLetter("abce");
 
- result
+ result;
