4991be91c1bdfa76b74e3a346080c209bfd441ce
WebDevolper1122 <sb2137842@gmail.com>
2023-02-16 22:36:51 +0500

    commit

---
+++ All-Problems/Intermidiate-Algorithims/missingLetters.js
@@ -0,0 +1,15 @@
+/*  
+Missing Letters
+*/
+
+function fearNotLetter(str) {
+    let alphabets = "abcdefghijklmnopqrstuvwxyz";
+    let startingPostion = alphabets.indexOf(str[0]);
+    let focusedElement  = alphabets.slice(startingPostion);
+    focusedElement
+
+  }
+  
+ let result =  fearNotLetter("abce");
+
+ result
