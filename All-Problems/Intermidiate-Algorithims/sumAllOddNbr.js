34377f45e04758952bc284f252f1ea53c302a76a
WebDevolper1122 <sb2137842@gmail.com>
2023-02-19 20:04:18 +0500

    commit

--- All-Problems/Intermidiate-Algorithims/sumAllOddNbr.js
+++ All-Problems/Intermidiate-Algorithims/sumAllOddNbr.js
@@ -3,17 +3,22 @@ Sum All Odd Fibonacci Numbers
 */
 
 function sumFibs(num) {
- let a = 0;
- let b = 1;
- for (let i=1; i<=num; i++){
-
-    if(b%2 === 1){
-        a+=b
+    let sum = 0;
+    let fib = 1;
+    let prevFib = 0;
+  
+    for (let i = 1; fib <= num; i++) {
+      if (fib % 2 === 1) {
+        sum += fib;
+      }
+  
+      let temp = fib;
+      fib += prevFib;
+      prevFib = temp;
     }
-    
-
- }
+  
+    return sum;
   }
   
   let result = sumFibs(4);
-  result
+  result;
