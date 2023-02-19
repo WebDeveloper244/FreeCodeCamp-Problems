b608e9a233617c182c202caed12e7aa09355d4f6
WebDevolper1122 <sb2137842@gmail.com>
2023-02-19 22:51:30 +0500

    commit

--- All-Problems/interViewQ/findLargeNbrInArray.js
+++ All-Problems/interViewQ/findLargeNbrInArray.js
@@ -5,7 +5,7 @@ Find Largest and Smallest Number in Array
 let arr1 = [32,67,8,1,9,87];
 
 function minimumNbr(arr1){ 
-   return arr1.reduce((minimumNbr,nbr)=> Math.min(minimumNbr, nbr)) 
+   return arr1.reduce((mini,nbr)=> Math.min(mini,nbr))
 }
 console.log(minimumNbr(arr1));
 
