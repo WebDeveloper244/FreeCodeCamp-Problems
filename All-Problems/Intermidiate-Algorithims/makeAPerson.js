73b427e7ddeed68ca6a9be0bd4185d228c2b81c5
WebDevolper1122 <sb2137842@gmail.com>
2023-02-24 22:12:02 +0500

    commit

--- All-Problems/Intermidiate-Algorithims/makeAPerson.js
+++ All-Problems/Intermidiate-Algorithims/makeAPerson.js
@@ -6,11 +6,18 @@ const Person = function(firstAndLast) {
     // Only change code below this line
     // Complete the method below and implement the others similarly
     this.getFullName = function() {
-      return "";
+      return firstAndLast
     };
-    return firstAndLast;
+    this.getFirstName = function() {
+      return firstAndLast.split(' ')[0];
+    };
+    this.getLastName = function() {
+      return firstAndLast.split(' ')[1];
+    };
   };
   
   const bob = new Person('Bob Ross');
-  bob.getFullName();
-  console.log(bob.getFullName());
+  // bob.getFullName(); 
+  // bob.getFirstName();
+  bob.getFirstName();
+  console.log(bob.getLastName());
